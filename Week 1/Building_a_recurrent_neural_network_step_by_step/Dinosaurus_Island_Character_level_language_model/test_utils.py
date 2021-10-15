f57330b84bfdc2542f9f4e8ae91ea319a2e8d0cc
rajatstats <91156074+rajatstats@users.noreply.github.com>
2021-10-15 08:44:57 +0530

    Add files via upload

---
+++ Week 1/Building_a_recurrent_neural_network_step_by_step/Dinosaurus_Island_Character_level_language_model/test_utils.py
@@ -0,0 +1,52 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "0516cbfd-83ab-43be-8c8b-a059a51a5eb9",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def rnn_cell_forward_test(rnn_cell_forward):\n",
+    "    raise NotImplementedError\n",
+    "\n",
+    "\n",
+    "def clip_test(clip):\n",
+    "    raise NotImplementedError\n",
+    "\n",
+    "\n",
+    "def sample_test(sample):\n",
+    "    raise NotImplementedError\n",
+    "\n",
+    "\n",
+    "def optimize_test(optimize):\n",
+    "    raise NotImplementedError\n",
+    "\n",
+    "\n",
+    "def model_test(model):\n",
+    "    raise NotImplementedError\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
